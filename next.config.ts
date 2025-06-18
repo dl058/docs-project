2c23cff77301a3c22c3431199b365a3df8ef8a1a
leeza dinh <dinh.leeza058@gmail.com>
2025-06-18 19:45:20 -0400

    switch to next.config.js and ignore eslint in production

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ This prevents ESLint from blocking your Vercel deploy
-  },
-};
-
-export default nextConfig;
