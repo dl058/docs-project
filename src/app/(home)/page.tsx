1d440dff4477dd6faff51e368c2b16c1ecfcab8d
leeza dinh <dinh.leeza058@gmail.com>
2025-06-21 12:41:46 -0400

    pink!

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,7 +17,10 @@ const Home = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
+      <div
+        className="fixed top-0 left-0 right-0 z-10 h-16 bg-pink-200
+ p-4"
+      >
         <Navbar />
       </div>
       <div className="mt-16">
